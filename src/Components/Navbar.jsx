fde7a861d5946ca022db70bf7f4708e0329ca52f
Yash Aghane <128385210+yashaghane21@users.noreply.github.com>
2023-04-14 20:25:25 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
     <div>
               <nav className="navbar navbar-expand-lg navbar-light " style={{ backgroundColor:'#113F67'}} >
               <div className="container-fluid text-white">
-                  <Link className="navbar-brand text-white" to="/"><b>ByteBlogs</b></Link>
+                  <Link className="navbar-brand text-white" to="/"><b>ByteBlogs ❤️</b></Link>
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                       <span className="navbar-toggler-icon"></span>
                   </button>
