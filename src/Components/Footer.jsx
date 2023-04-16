a1bf5595b7b5ac29f067f02dcf985c1742ac2ea3
Yash Aghane <128385210+yashaghane21@users.noreply.github.com>
2023-04-16 11:07:02 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Footer() {
   return (
       <footer className="blog-footer d-flex justify-content-center p-4 w-100" style={{ backgroundColor: '#113F67',color:'white',marginTop:'10%'}}>
-          <p>&copy; 2023 ByteDevs Developed by ByteDevs Team </p>
+          <p>&copy; 2023 Developed by ByteDevs Team </p>
           <p>
               
           </p>
